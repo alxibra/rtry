be63c1ca560b90de64c7ea1c0ab4fbcd7bdd60a1
alxibra <alxibra@forstok.com>
2025-08-04 15:27:06 +0700

    add go.mod go.sum and examples

---
+++ examples/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"math"
+
+	"github.com/alxibra/rtry"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, chn := initialize()
+	defer conn.Close()
+	defer chn.Close()
+
+	rty, err := rtry.Init(
+		"main_exchange",
+		"main_queue",
+		"retry_queue",
+		"main_key",
+		"retry_key",
+		rtry.Option{
+			"max-attempts": 3,
+			"backoff": func(retryCount int) int {
+				return int(math.Pow(float64(retryCount), 4) + 5)
+
+			},
+		},
+		chn,
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	msgs, _ := rty.Consume(chn)
+
+	log.Println("Ready to consume messages. Press Ctrl+C to exit.")
+	for msg := range msgs {
+		log.Printf("Received message: %s", msg.Body)
+		rty.Retry(
+			msg,
+			nil,
+			chn,
+		)
+		msg.Ack(false)
+	}
+}
+
+func initialize() (*amqp.Connection, *amqp.Channel) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnErr(err, "Failed to connect to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnErr(err, "Failed to open a channel")
+	return conn, ch
+}
+
+func failOnErr(err error, msg string) {
+	if err != nil {
+		log.Fatalf("💥 %s: %s", msg, err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/alxibra/rtry
+
+go 1.24.1
+
+require (
+	github.com/fatih/color v1.18.0
+	github.com/rabbitmq/amqp091-go v1.10.0
+	golang.org/x/exp v0.0.0-20250718183923-645b1fa84792
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
