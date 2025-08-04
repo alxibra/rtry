3d56bcfee55a493b9eb6af07a4f97058bf69355a
alxibra <alxibra@forstok.com>
2025-08-04 15:43:21 +0700

    add examples folder

--- examples/main.go
+++ examples/main.go
@@ -9,6 +9,58 @@ import (
 )
 
 func main() {
+	// withCompleteOption()
+	withoutCompleteOption()
+}
+
+func initialize() (*amqp.Connection, *amqp.Channel) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnErr(err, "Failed to connect to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnErr(err, "Failed to open a channel")
+	return conn, ch
+}
+
+func failOnErr(err error, msg string) {
+	if err != nil {
+		log.Fatalf("💥 %s: %s", msg, err)
+	}
+}
+
+func withoutCompleteOption() {
+	conn, chn := initialize()
+	defer conn.Close()
+	defer chn.Close()
+
+	rty, err := rtry.Init(
+		"main_exchange",
+		"main_queue",
+		"retry_queue",
+		"main_key",
+		"retry_key",
+		nil,
+		chn,
+	)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	msgs, _ := rty.Consume(chn)
+
+	log.Println("Ready to consume messages. Press Ctrl+C to exit.")
+	for msg := range msgs {
+		log.Printf("Received message: %s", msg.Body)
+		rty.Retry(
+			msg,
+			nil,
+			chn,
+		)
+		msg.Ack(false)
+	}
+}
+
+func withCompleteOption() {
 	conn, chn := initialize()
 	defer conn.Close()
 	defer chn.Close()
@@ -40,23 +92,12 @@ func main() {
 		log.Printf("Received message: %s", msg.Body)
 		rty.Retry(
 			msg,
-			nil,
+			rtry.Option{
+				"delay_in_second": 5,
+			},
 			chn,
 		)
 		msg.Ack(false)
 	}
-}
 
-func initialize() (*amqp.Connection, *amqp.Channel) {
-	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	failOnErr(err, "Failed to connect to RabbitMQ")
-	ch, err := conn.Channel()
-	failOnErr(err, "Failed to open a channel")
-	return conn, ch
-}
-
-func failOnErr(err error, msg string) {
-	if err != nil {
-		log.Fatalf("💥 %s: %s", msg, err)
-	}
 }
